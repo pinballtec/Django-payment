fde0f40058e6f86293615834c83e9aaa0274b50a
Daniil_Boika <daniilboiko99@gmail.com>
2022-06-01 21:34:03 +0200

    basic template/views configuration

--- project/app/urls.py
+++ project/app/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.test, name='test'),
+    path('success/<str:args>/', views.success, name='success'),
+    path('', views.test, name='index'),
+    path('charge/', views.charge, name='charge'),
 ]
--- project/app/views.py
+++ project/app/views.py
@@ -1,7 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.urls import reverse
 
 # Create your views here.
 
 
 def test(request):
     return render(request, 'app/template.html')
+
+
+def charge(request):
+    amount = 5
+    if request.method == 'POST':
+        print('Data:', request.POST)
+
+    return redirect(reverse('ok', args=[amount]))
+
+
+def success(request, args):
+    amount = args
+    return render(request, 'app/succes.html', {'amount': amount})
