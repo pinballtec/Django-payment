8bff0e56618a74489e326d84fd215e2404f8016d
Daniil_Boika <daniilboiko99@gmail.com>
2022-06-17 16:01:05 +0200

    views fucntions for demo store are complited

--- project/pay_pal/views.py
+++ project/pay_pal/views.py
@@ -12,3 +12,20 @@ from .models import Product, Order
 
 def simpleCheckout(request):
 	return render(request, 'pay_pal/simple-checkout.html')
+
+
+def demo_store(request):
+	products = Product.objects.all()
+	context = {'products': products}
+	return render(request, 'pay_pal/demo_store.html', context)
+
+
+def checkout(request, pk):
+	product = Product.objects.get(id=pk)
+	context = {'products': product}
+	return render(request, 'pay_pal/product_checkout.html')
+
+def paymentComplete(request):
+	body = json.loads(request.body)
+	print('BODY:', body)
+	return JsonResponse('Payment completed!', safe=False)
